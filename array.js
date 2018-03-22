bf4360609d6b7774ecc8b8fd715ad6a8c7d8f9c5
moryw <matt@skitskat.xyz>
2018-03-22 10:22:12 -0400

    assign var to the string position in an array

--- array.js
+++ array.js
@@ -1,1 +1,9 @@
 let newYearsResolutions = ['learn to code', 'learn javascript', 'conquer the world'];
+
+console.log(newYearsResolutions);
+
+let listItem = newYearsResolutions[0];
+
+console.log(listItem);
+console.log(newYearsResolutions[2]);
+console.log(newYearsResolutions[3]);
