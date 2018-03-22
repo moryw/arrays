898a499c1675ecaa8ea82d553022f7f8380fb41d
moryw <matt@skitskat.xyz>
2018-03-22 10:45:28 -0400

    Add .shift(), .unshift(), and .slice() methods to array

--- array.js
+++ array.js
@@ -1,18 +1,28 @@
-let newYearsResolutions = ['learn to code', 'learn javascript', 'conquer the world'];
+//learn about arrays and some basic methods
+// let newYearsResolutions = ['learn to code', 'learn javascript', 'conquer the world'];
+//
+// console.log(newYearsResolutions);
+//
+// let listItem = newYearsResolutions[0];
+//
+// console.log(listItem);
+// console.log(newYearsResolutions[2]);
+// console.log(newYearsResolutions[3]);
+//
+// newYearsResolutions[1] = 'learn a new language';
+//
+// console.log(newYearsResolutions.length);
+//
+// newYearsResolutions.push('newItem1', 'newItem2');
+// console.log(newYearsResolutions);
+//
+// newYearsResolutions.pop();
 
-console.log(newYearsResolutions);
+//test some methods on a gracery list
+let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
 
-let listItem = newYearsResolutions[0];
+groceryList.shift(); //rm first item from beginning of list
+groceryList.unshift('popcorn'); //add to beginning of list
+console.log(groceryList.slice(1,4));
 
-console.log(listItem);
-console.log(newYearsResolutions[2]);
-console.log(newYearsResolutions[3]);
-
-newYearsResolutions[1] = 'learn a new language';
-
-console.log(newYearsResolutions.length);
-
-newYearsResolutions.push('newItem1', 'newItem2');
-console.log(newYearsResolutions);
-
-newYearsResolutions.pop();
+console.log(groceryList);
